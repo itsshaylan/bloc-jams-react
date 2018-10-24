9e0ae1128a33f1a2751c311728d7f9a795445f53
Shayla <itsshaylan@gmail.com>
2018-10-24 17:50:21 -0500

    Linking to albums

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,4 +1,5 @@
  import React, { Component } from 'react';
+ import { Link } from 'react-router-dom';
  import albumData from './../data/albums';
 
  class Library extends Component { 
@@ -11,13 +12,13 @@
     return ( 
       <section className='library'>
         {
-          this.state.albums.map( (album, index) => 
-            <div key={index} >
+        	this.state.albums.map( (album, index) => 
+  			<Link to={`/album/${album.slug}`} key={index}>
                <img src={album.albumCover} alt={album.title} />
                <div>{album.title}</div>
                <div>{album.artist}</div>
                <div>{album.songs.length} songs</div>
-            </div>
+            </Link>
           )
         }
       </section>
