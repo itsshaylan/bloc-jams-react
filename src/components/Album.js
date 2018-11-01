cb5738c3193660138ec5c6b24bbb019897a78934
Shayla <itsshaylan@gmail.com>
2018-11-01 14:04:34 -0500

    passing down handleTimeChange as props

--- src/components/Album.js
+++ src/components/Album.js
@@ -150,6 +150,7 @@
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
            handleNextClick={() => this.handleNextClick()}
+           handleTimeChange={(e) => this.handleTimeChange(e)}
          />
        </section>
      );
